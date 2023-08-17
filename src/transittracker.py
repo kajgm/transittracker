e77c0ac0a7176b65c1e23f42cf8c416ae961f041
Kaj Grant <kajgrant@gmail.com>
2023-08-17 14:20:27 -0700

    enable display

--- src/transittracker.py
+++ src/transittracker.py
@@ -11,28 +11,28 @@ DISPLAY_FLAG = False
 
 
 def main(args):
-
     tApi = transitApi()
 
-    if (DISPLAY_FLAG):
+    if DISPLAY_FLAG:
         dsp = display(tApi)
+        dsp.show_display()
 
-    while (not DISPLAY_FLAG):
-
+    while not DISPLAY_FLAG:
         res = tApi.get_stop_info()
         resJson = res.json()[0]
-        closestSchedule = resJson['Schedules'][0]
+        closestSchedule = resJson["Schedules"][0]
 
-        print_status(closestSchedule['ExpectedCountdown'],
-                     closestSchedule['ExpectedLeaveTime'],
-                     closestSchedule['ScheduleStatus'],
-                     closestSchedule['LastUpdate'])
+        print_status(
+            closestSchedule["ExpectedCountdown"],
+            closestSchedule["ExpectedLeaveTime"],
+            closestSchedule["ScheduleStatus"],
+            closestSchedule["LastUpdate"],
+        )
 
         wait(WAIT_TIME)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--display", required=False)
     args = parser.parse_args()
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     try:
         main(args)
     except KeyboardInterrupt:
-        print('\nExiting')
+        print("\nExiting")
         try:
             sys.exit(0)
         except SystemExit:
