04fc1179e8afe84b8565cda9ddd43477e802c2d0
Kaj Grant <kajgrant@gmail.com>
2023-08-17 12:00:21 -0700

    fix: class definition issues

--- src/display.py
+++ src/display.py
@@ -5,7 +5,6 @@ from helpers import *
 
 
 class display:
-
     root = None
     txt = None
     fnt = None
@@ -13,13 +12,18 @@ class display:
     tApi = None
 
     def __init__(self, api):
-        root = Tk()
-        txt = StringVar()
-        fnt = font.Font(family='Helvetica', size=35, weight='bold')
-        lbl = ttk.Label(root, textvariable=txt, font=fnt,
-                        foreground="white", background="black")
-        lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
-        tApi = api
+        self.root = Tk()
+        self.txt = StringVar()
+        self.fnt = font.Font(family="Helvetica", size=35, weight="bold")
+        self.lbl = ttk.Label(
+            self.root,
+            textvariable=self.txt,
+            font=self.fnt,
+            foreground="white",
+            background="black",
+        )
+        self.lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.tApi = api
 
     def quit(self, *args):
         self.root.destroy()
@@ -27,13 +31,13 @@ class display:
     def show_time(self):
         res = self.tApi.get_stop_info()
         resJson = res.json()[0]
-        closestSchedule = resJson['Schedules'][0]
-        self.txt.set(closestSchedule['ExpectedLeaveTime'])
-        self.root.after(40*1000, self.show_time)
+        closestSchedule = resJson["Schedules"][0]
+        self.txt.set(closestSchedule["ExpectedLeaveTime"])
+        self.root.after(40 * 1000, self.show_time)
 
     def show_display(self):
         self.root.attributes("-fullscreen", True)
-        self.root.config(background='black', cursor="none")
+        self.root.config(background="black", cursor="none")
         self.root.bind("x", quit)
         self.root.after(1000, self.show_time)
 
